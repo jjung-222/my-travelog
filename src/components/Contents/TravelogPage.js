5cc03f8ffd5eb298728da415c13daf3bf54a1315
이연정 <jjung222@douzone.com>
2023-08-10 15:08:06 +0900

    git test22222

--- src/components/Contents/TravelogPage.js
+++ src/components/Contents/TravelogPage.js
@@ -22,7 +22,7 @@ export default function TravelogPage() {
     }
 
     const onInputChangeMemo = (e) => {
-        //git test ddd
+        //git test dddgit
         setLog({...log, children: e.target.value})
     }
 
